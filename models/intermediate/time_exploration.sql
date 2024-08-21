0d6c3d8efda1ca7b4f7d86e4677a637478b89b40
Dominique Lewagon <dominique.lewagon@gmail.com>
2024-08-21 13:53:44 +0000

    Append month and day to temporality table

--- models/intermediate/time_exploration.sql
+++ models/intermediate/time_exploration.sql
@@ -7,17 +7,21 @@ WITH total AS (
 -- This intermediate for extract year count all rows and get percent 
 intermediate_date AS (SELECT
   EXTRACT(year FROM release_date) AS year,
+  EXTRACT(month FROM release_date) AS month,
+  EXTRACT(day FROM release_date) AS day,
   COUNT(imdb_id) AS nb_movies,
   ROUND(SAFE_DIVIDE(COUNT(imdb_id), MAX(total.total_movies)) * 100,3) AS Percent_total,
   genre_1
 FROM {{ ref('datequeries') }}
 CROSS JOIN total
-GROUP BY year,genre_1
-ORDER BY year
+GROUP BY year, month, day, genre_1
+ORDER BY year,month,day
 )
 -- Get year, genre count and create a new column case when for segment all count
 SELECT
 year,
+month,
+day,
 CASE 
     WHEN year BETWEEN 1980 AND 1989 THEN '1980'
     WHEN year BETWEEN 1990 AND 1999 THEN '1990'
@@ -30,6 +34,6 @@ genre_1,
 nb_movies,
 Percent_total
 FROM intermediate_date
-GROUP BY year,Decades,genre_1,nb_movies,Percent_total
+GROUP BY year,month,day, Decades,genre_1,nb_movies,Percent_total
 ORDER BY year
 -------------------------------------------------------------------------------------------------
