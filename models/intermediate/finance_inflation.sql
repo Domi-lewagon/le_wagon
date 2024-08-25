74af10ad57a39889cba4d8f094014b7166044186
Riwan Kessaci <riwan.kess@hotmail.fr>
2024-08-25 12:36:39 +0000

    upload table

--- models/intermediate/finance_inflation.sql
+++ models/intermediate/finance_inflation.sql
@@ -1,4 +1,20 @@
+WITH finance_cip_join AS(
 SELECT *
 FROM {{ ref('finance') }} AS fi
-LEFT JOIN {{ ref('stg_dbt-project-430308__cpi_inflation_year_index') }} AS CPI
-ON fi.startYear=  CAST(CPI.year,int)
+INNER JOIN {{ ref('stg_dbt-project-430308__cpi_inflation_year_index') }} AS CPI
+    ON fi.startYear = CPI.year)
+
+SELECT
+imdb_id,
+title,
+budget,
+box_office,
+benefice,
+ROI,
+imdb_rating,
+imdb_votes,
+release_date,
+startYear,
+runtime,
+genre_1,
+FROM finance_cip_join
