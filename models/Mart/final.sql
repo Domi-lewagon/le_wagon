919530d1c25d6420dcc70df606a9a3eb83eb5f72
Dominique Lewagon <dominique.lewagon@gmail.com>
2024-08-27 15:57:11 +0000

    decades

--- models/Mart/final.sql
+++ models/Mart/final.sql
@@ -4,6 +4,9 @@ fc.directors,
 budget_adjusted,
 box_office_adjusted,
 benefice_adjusted,
+te.Decades
 FROM {{ ref('datequeries') }} as d 
 LEFT JOIN {{ ref('finance_complete') }} fc 
+USING (imdb_id)
+LEFT JOIN {{ ref('time_exploration') }} as te 
 USING (imdb_id)
