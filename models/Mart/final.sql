8b6b0c518f0e711dc542daa3ee97f26c73bf3865
Dominique Lewagon <dominique.lewagon@gmail.com>
2024-08-27 18:24:11 +0000

    Add runtime class on table

--- models/Mart/final.sql
+++ models/Mart/final.sql
@@ -4,9 +4,12 @@ fc.directors,
 budget_adjusted,
 box_office_adjusted,
 benefice_adjusted,
-te.Decades
+te.Decades,
+ar.runtime_class
 FROM {{ ref('datequeries') }} as d 
 LEFT JOIN {{ ref('finance_complete') }} fc 
 USING (imdb_id)
 LEFT JOIN {{ ref('time_exploration') }} as te 
+USING (imdb_id)
+LEFT JOIN {{ ref('average_runtime') }} as ar 
 USING (imdb_id)
